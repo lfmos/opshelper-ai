a047ef1ac017f2dac446c6446a99824f62f07e0f
Luís Filipe Medeiros <lfmedeiros.rj@gmail.com>
2026-07-06 20:04:02 -0300

    Melhora algoritmo de busca do assistente

--- src/assistant.py
+++ src/assistant.py
@@ -1,28 +1,75 @@
 from src.knowledge import carregar_base_conhecimento
 
 
+def encontrar_palavras_chave(texto):
+
+    palavras = texto.lower().split()
+
+    palavras_remover = [
+        "o",
+        "a",
+        "os",
+        "as",
+        "um",
+        "uma",
+        "como",
+        "meu",
+        "minha",
+        "não",
+        "nao",
+        "está",
+        "esta"
+    ]
+
+    return [
+        palavra
+        for palavra in palavras
+        if palavra not in palavras_remover
+    ]
+
+
 def buscar_resposta(pergunta):
 
     base = carregar_base_conhecimento()
 
-    pergunta = pergunta.lower()
+    palavras_usuario = encontrar_palavras_chave(pergunta)
+
+
+    melhor_resultado = None
+    maior_pontuacao = 0
+
 
     for item in base:
 
-        termos = [
-            item["categoria"],
-            item["problema"]
-        ]
+        texto_base = (
+            item["categoria"]
+            + " "
+            + item["problema"]
+        ).lower()
 
-        for termo in termos:
 
-            if termo.lower() in pergunta:
+        pontuacao = 0
 
-                return {
-                    "categoria": item["categoria"],
-                    "resposta": item["solucao"],
-                    "proximo_passo": item["proximo_passo"]
-                }
+
+        for palavra in palavras_usuario:
+
+            if palavra in texto_base:
+                pontuacao += 1
+
+
+        if pontuacao > maior_pontuacao:
+            maior_pontuacao = pontuacao
+            melhor_resultado = item
+
+
+    if melhor_resultado and maior_pontuacao > 0:
+
+        return {
+            "categoria": melhor_resultado["categoria"],
+            "resposta": melhor_resultado["solucao"],
+            "proximo_passo": melhor_resultado["proximo_passo"]
+        }
+
 
     return {
         "categoria": "Não identificado",
